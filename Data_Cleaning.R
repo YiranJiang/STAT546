3e8fee903da2460b1fb3875a45be62e537a2b0eb
wang1190 <53959868+wang1190@users.noreply.github.com>
2020-05-05 14:27:21 -0400

    Add files via upload

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -1,7 +1,6 @@
 library(dplyr)
 
-setwd('/Users/jiangyiran/Desktop/Purdue/2020Spring/546/project/covid-19')
-data <- read.csv('./latestdata.csv')
+data <- read.csv('/Users/wang/Desktop/546 Project/latestdata.csv')
 
 dim(data)
 colnames(data)
@@ -24,11 +23,44 @@ symp_vec <- gsub("[[:punct:]]", "", symp_vec)
 symp_vec <- gsub("[0-9]", "", symp_vec)
 
 ## See all Unique Symptoms
-unique(symp_vec)
+as.data.frame(unique(symp_vec))
 
 ## Check Specific Key words
 symp_vec[grep('fever',symp_vec)]
 data$symptoms[grep('fever',data$symptoms)]
+
+## Add new column for clean data
+data$symptoms_clean <- rep("",nrow(data))
+
+data$symptoms_clean[grep('rever|febre|fever|feverish',data$symptoms)] <- "Fever"
+data$symptoms_clean[grep('cough|couh|throat|throa|sputum|phlegm|pharyngitis|pharyngeal|expectoration|tosse',data$symptoms)] <- lapply(data$symptoms_clean[grep('cough|couh|throat|throa|sputum|phlegm|pharyngitis|pharyngeal|expectoration|tosse',data$symptoms)], function(x) paste("Cough",x,sep=","))
+data$symptoms_clean[grep('respirat|breathing|dyspnea|dsypnea|breath|tight|anhelation|distress|wheezing|gasp|grasp|hypoxia',data$symptoms)] <- lapply(data$symptoms_clean[grep('respirat|breathing|dyspnea|dsypnea|breath|tight|anhelation|distress|wheezing|gasp|grasp|hypoxia',data$symptoms)], function(x) paste("Dyspnea",x,sep=","))
+data$symptoms_clean[grep('pain|ach|body|limbs|muscle|muscular|myalgia|mialgia',data$symptoms)] <- lapply(data$symptoms_clean[grep('pain|ach|body|limbs|muscle|muscular|myalgia|mialgia',data$symptoms)], function(x) paste("Body Pain",x,sep=","))
+data$symptoms_clean[grep('anorexia|nausea|mausea|esophageal|vomit|inappetance|inapatence|pharyngeal|pharynx|emesis|dysphagia',data$symptoms)] <- lapply(data$symptoms_clean[grep('anorexia|nausea|mausea|esophageal|vomit|inappetance|inapatence|pharyngeal|pharynx|emesis|dysphagia',data$symptoms)], function(x) paste("Anorexia",x,sep=","))
+data$symptoms_clean[grep('nasal|nose|sneez|rhinorrhea|rhinorrhoea|coriza',data$symptoms)] <- lapply(data$symptoms_clean[grep('nasal|nose|sneez|rhinorrhea|rhinorrhoea|coriza',data$symptoms)], function(x) paste("Rhinorrhea",x,sep=","))
+data$symptoms_clean[grep('cold|chill',data$symptoms)] <- lapply(data$symptoms_clean[grep('cold|chill',data$symptoms)], function(x) paste("Chills",x,sep=","))
+data$symptoms_clean[grep('diarrhea|diarrhoea|diarrheoa',data$symptoms)] <- lapply(data$symptoms_clean[grep('diarrhea|diarrhoea|diarrheoa',data$symptoms)], function(x) paste("Diarrhea",x,sep=","))
+data$symptoms_clean[grep('asymptomatic|afebrile|no serious symptoms|none|oligosymptomatic|no symptoms|no clinical symptoms',data$symptoms)] <- lapply(data$symptoms_clean[grep('asymptomatic|afebrile|no serious symptoms|none|oligosymptomatic|no symptoms|no clinical symptoms',data$symptoms)], function(x) paste("Asymptomatic",x,sep=","))
+data$symptoms_clean[grep('kidney|renal',data$symptoms)] <- lapply(data$symptoms_clean[grep('kidney|renal',data$symptoms)], function(x) paste("Kidney Injury",x,sep=","))
+data$symptoms_clean[grep('heart|cardiogenic|cardiac|myocardial|cardiopulmonary',data$symptoms)] <- lapply(data$symptoms_clean[grep('heart|cardiogenic|cardiac|myocardial|cardiopulmonary',data$symptoms)], function(x) paste("Heart Failure",x,sep=","))
+data$symptoms_clean[grep('septic',data$symptoms)] <- lapply(data$symptoms_clean[grep('septic',data$symptoms)], function(x) paste("Septic Shock",x,sep=","))
+data$symptoms_clean[grep('^mild$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^weakness$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^severe$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^pneumonia$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^pulmonary$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^fatigue$',data$symptoms)] <- "Missing"
+data$symptoms_clean[grep('^discomfort$',data$symptoms)] <- "Missing"
+
+data$symptoms_clean[grep('mild:moderate|mild to moderate|mild symptoms|covid|pnuemonia|pneumonitis|lesions|physical|yes|flulike|somnolence|hcovbrazilspbr|myelofibrosis',data$symptoms)] <- "Missing"
+
+## Cleaned symptoms
+u<-unique(data$symptoms_clean)
+
+## Classified Unique Symptoms
+us <- as.character(data$symptoms_clean)
+us_vec <- unlist(strsplit(us,','))
+unique(us_vec)
 
 ## Extract gender data
 data$sex_clean<-rep(NA,nrow(data))
@@ -62,3 +94,6 @@ data$age_clean[age>-1&age<20]<-0
 data$age_clean[age>20&age<40]<-1
 data$age_clean[age>40&age<60]<-2
 data$age_clean[age>60]<-3
+
+
+

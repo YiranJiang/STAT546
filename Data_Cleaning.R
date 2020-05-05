b06409fb9d0619bfc34caf989038854a734fe4d4
EadwinusCantonensis <41934537+EadwinusCantonensis@users.noreply.github.com>
2020-05-05 02:51:42 -0400

    Update Data_Cleaning.R

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -47,3 +47,18 @@ data$outcome_clean[grep('discharged|Discharged|recovered|discharge',data$outcome
 data$outcome_clean[grep('death|dead|died|Deceased',data$outcome)]<-1
 data$outcome_clean[grep('treated in an intensive care unit',data$outcome)]<-2
 data$outcome_clean[grep('Receiving Treatment|stable',data$outcome)]<-3
+
+## Extract Age data
+age<-data$age
+as.data.frame(unique(age))
+data$age_clean<-rep(NA,nrow(data))
+# 0: 0-19 1:20-39 2:40-59 3:60+
+age[grep('40-49|50-59',data$age)]<-50
+age[grep('20-29|30-35|30-39',data$age)]<-30
+age[grep('80-89|60-69|70-79|60-60|65-|90-99',data$age)]<-90
+age[grep('0-6|0-10|13-19',data$age)]<-10
+age<-as.numeric(as.character(age))
+data$age_clean[age>-1&age<20]<-0
+data$age_clean[age>20&age<40]<-1
+data$age_clean[age>40&age<60]<-2
+data$age_clean[age>60]<-3

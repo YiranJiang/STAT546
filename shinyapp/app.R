270c5b55e00416e6724cb9f47519a6759344c57e
JudyCom <40449861+JudyCom@users.noreply.github.com>
2020-05-07 18:37:50 -0400

    Add files via upload

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,4 +1,5 @@
-list_of_packages <- c("shiny", "png", "visNetwork", "rsconnect")
+list_of_packages <- c("shiny", "png", "visNetwork", "rsconnect",
+                      "shinythemes", "shinycssloaders")
 new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
 if(length(new_packages)) install.packages(new_packages)
 
@@ -25,50 +26,39 @@ node <- data.frame("id" = c(paste("s0", 1:9, sep = ""),
                    "symptom" = c("asymptomatic", "gender", "age",
                                  "dyspnea", "kidney injury",
                                  "septic shock", "heart failure",
-                                 "fever", "cough", "anorxia",
+                                 "fever", "cough", "anorexia",
                                  "chills", "body pain",
                                  "diarrhea", "rhinorrhea"),
                    "symptom_type" = c("asymptomatic", rep("basic info", 2),
                                       rep("severe symptoms", 4),
                                       rep("mild symptoms", 7)),
-                   "symptom_size" = c(3, rep(3,2),rep(5,4), rep(7,7)))
+                   "symptom_size" = c(rep(3,3),rep(5,11)))
 
 link <- data.frame("from" = c("s03", "s03", "s02", "s02",
-                              "s04","s06", "s08","s09",
-                              "s11", "s12", "s14",
-                              "s08", "s12", "s13", "s14"), 
+                              "s04", "s08", "s08","s09",
+                              "s09","s10","s11", "s12",
+                              "s12", "s13","s14"), 
                    "to" = c("s01", "s04", "s08", "s14",
-                            "s08","s09", "s06","s07",
-                            "s12", "s04", "s12",
-                            "s12", "s08","s14", "s13"),
-                   #"type" = c(),
-                   "weight" = 1:15)
-
-visNetwork(node, link, width="500px", height="1000px", background="#eeefff",
-           main="Network", submain="BN for Symptoms",
-           footer= "Bayesian Network for Symptoms")%>% 
-  visNodes(shapeProperties = list(useBorderWithImage = TRUE))
+                            "s05", "s04", "s06", "s06",
+                            "s07", "s08", "s12", "s08",
+                            "s04", "s14","s12"))
 
 vis.nodes <- node
 vis.links <- link
 
-# vis.nodes$shape  <- "dot"  
 vis.nodes$shadow <- TRUE # Nodes will drop shadow
 vis.nodes$title  <- vis.nodes$symptom # Text on click
 vis.nodes$label  <- vis.nodes$symptom # Node label
-vis.nodes$size   <- vis.nodes$symptom_size*5 # Node size
+vis.nodes$size   <- vis.nodes$symptom_size*7 # Node size
 vis.nodes$borderWidth <- 2 # Node border width
 
-colrs <- c("lightblue", "lightgreen", "orange", "red")
-vis.nodes$color.background <- colrs[as.factor(node$symptom_type)]
+
 vis.nodes$color.border <- "black"
-vis.nodes$color.highlight.background <- "orange"
 vis.nodes$color.highlight.border <- "darkred"
 
-vis.links$width <- 1+link$weight/8 # line width
 vis.links$color <- "gray"    # line color  
 vis.links$arrows <- "to" # arrows: 'from', 'to', or 'middle'
-vis.links$smooth <- FALSE    # should the edges be curved?
+vis.links$smooth <- FALSE   # should the edges be curved?
 vis.links$shadow <- FALSE    # edge shadow
 
 
@@ -79,11 +69,12 @@ ui <- navbarPage(
   title = "Shiny App", 
   tabPanel(title = "Tab 1",
            sidebarPanel(
-            h1("COVID-19"),
-            a(href="http://www.google.com", "Data Source"), # temp 
-            img(height=100, width=1051/428*100,
-                     src="covid-virus.png")
-            ),
+             h1("COVID-19"),
+             a(href="https://github.com/beoutbreakprepared/nCoV2019/tree/master/latest_data",
+               "Data Source"), # temp 
+             img(height=100, width=1051/428*100,
+                 src="covid-virus.png")
+           ),
            mainPanel(
              visNetworkOutput("network")
            )
@@ -95,7 +86,7 @@ ui <- navbarPage(
 server <- function(input, output){  #assemble input into output
   
   output$network <- renderVisNetwork({
-    visOptions(visNetwork(vis.nodes, vis.links, main = "BN of Symptoms"),
+    visOptions(visNetwork(vis.nodes, vis.links, main = "Bayesian Network of Symptoms"),
                highlightNearest = TRUE, selectedBy = "symptom_type")
   })
   
